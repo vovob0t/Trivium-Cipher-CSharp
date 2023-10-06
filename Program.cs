ff7927d03f33c8a9e3ccbe88297dfea4a572d668
vovobot <super.cuzm2014@yandex.ru>
2023-10-07 00:09:05 +0500

    Initil commit. Testing BitArray class and seting beging of a Trivium cipher

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+using System.Security.Cryptography;
+using Nito.Collections;
+
+namespace Trivium;
+
+
+/*
+use right shift for moving bits in array
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hexValue = "CB4579AD1CA63FB5F2AA";
+        Trivium.hexToBits(hexValue);
+        var test = new Deque<int>();
+        Trivium teswt = new("CB4579AD1CA63FB5F2AA", "CB4520FC1CA63F830D3A");
+
+
+
+       /* BigInteger decimalValue = BigInteger.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+
+        List<byte> bytes = (decimalValue.ToByteArray()).ToList();
+
+        BigInteger tes = 1;
+        BitArray bydtes = new(tes.ToByteArray());
+*/
+        /* Convert all (key, IV and others values like 0s and 1s) to bytes array,
+        piece them together and convert all to a BitArray */
+        test.AddToFront(5);
+        test.AddToFront(6);
+        test.AddToBack(7);
+
+        foreach (var item in test){
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Trivium.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.Numerics;
+using System.Text;
+
+namespace Trivium;
+
+public class Trivium
+{
+    private BitArray keyBits;
+    private BitArray IVBits;
+    private BitArray internalState;
+
+    public Trivium(string key, string IV)
+    {
+
+        internalState = stateInit(key, IV);
+        return;
+    }
+
+    private BitArray stateInit(string key, string IV)
+    {
+        BitArray state = new BitArray(length: 288);
+
+
+        bool[] keyBits = hexToBits(key);
+        bool[] IVBits = hexToBits(IV);
+
+        List<bool> allBitsState = new();
+
+        allBitsState.AddRange(keyBits.ToList());
+        allBitsState.AddRange(Enumerable.Repeat(false, 13));
+
+        allBitsState.AddRange(IVBits.ToList());
+        allBitsState.AddRange(Enumerable.Repeat(false, 4));
+
+        allBitsState.AddRange(Enumerable.Repeat(false, 108));
+        allBitsState.AddRange(Enumerable.Repeat(true, 3));
+
+
+        return new BitArray(allBitsState.ToArray());
+    }
+
+    static public bool[] hexToBits(string hexValue)
+    {
+        BigInteger decimalValue = BigInteger.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+
+        // Encoding enc = Encoding.ASCII;
+
+        // BitArray bitArray = new(Encoding.ASCII.GetBytes(hexValue));
+
+        BitArray bitArray = new(decimalValue.ToByteArray());
+
+        bool[] bits = new bool[bitArray.Length];
+
+        bitArray.CopyTo(bits, 0);
+        Array.Reverse(bits);
+
+        //        bitArray = new BitArray(bits);
+
+
+        // int breaker = 0;
+        // string buildBits = "";
+
+        // foreach (bool bite in bitArray)
+        // {
+        //     if (breaker == 4)
+        //     {
+        //         Console.Write(" ");
+        //         breaker = 0;
+        //     }
+        //     if (bite){
+        //         Console.Write(1);
+        //         buildBits+="1";
+        //     }
+        //     else {
+        //         Console.Write(0);
+        //          buildBits+="0";
+        //     }
+        //     breaker++;
+        // }
+
+        return bits;
+    }
+}
